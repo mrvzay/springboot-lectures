9dfe713851ee6d55c6ca1fed7f7c532f4416f695
mrvzay <vzay1999@gmail.com>
2024-07-26 19:40:55 +0530

    only jpa files added

---
+++ jpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
+
+}
